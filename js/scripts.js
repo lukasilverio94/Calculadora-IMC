9b7a1b38a9bde4e73cf1882f57299fa71400c82c
Lucas Silverio <117397454+lukasilverio94@users.noreply.github.com>
2023-06-23 17:41:57 +0200

    added some functions js

--- js/scripts.js
+++ js/scripts.js
@@ -38,7 +38,6 @@ const data = [
 
 // Selecao de elementos
 const imcTable = document.querySelector("#imc-table");
-
 const heightInput = document.querySelector("#height");
 const weightInput = document.querySelector("#weight");
 const calcBtn = document.querySelector("#calc-btn");
@@ -47,11 +46,51 @@ const clearBtn = document.querySelector("#clear-btn");
 // funcoes:
 function createTable(data) {
   data.forEach((item) => {
-
     const div = document.createElement("div");
     div.classList.add("table-data");
+    // criando os 3 parágrafos da div
+    const classification = document.createElement("p");
+    classification.innerText = item.classification;
+
+    const info = document.createElement("p");
+    info.innerText = item.info;
+
+    const obesity = document.createElement("p");
+    obesity.innerText = item.obesity;
+
+    //incluir paragrafos na div
+    div.appendChild(classification);
+    div.appendChild(info);
+    div.appendChild(obesity);
+
+    // colocar na tabela cada uma delas:
+    imcTable.appendChild(div);
   });
 }
-//inicializacao do meu projeto
 
-//Eventos qdo os botoes comecam funcionar baseado nos inputs do usuario
+// limpar inputs
+function cleanInputs() {
+  heightInput.value = "";
+  weightInput.value = "";
+}
+
+// LIMITAR CARACTERES PARA NUMEROS
+function validDigits(text) {
+  return text.replace(/[^0-9,]/g, "");
+}
+//inicializacao do meu projeto
+createTable(data);
+
+// Eventos
+[heightInput, weightInput].forEach((el) => {
+  el.addEventListener("input", (e) => {
+    const updatedValue = validDigits(e.target.value);
+
+    e.target.value = updatedValue;
+  });
+});
+
+clearBtn.addEventListener("click", (e) => {
+  e.preventDefault(); // pra parar de carregar a pagina ao clicar em Limpar, adicionar o preventDefault() em qualquer evento
+  cleanInputs(); //chama funcao pra limpar imputs, acima.
+});
