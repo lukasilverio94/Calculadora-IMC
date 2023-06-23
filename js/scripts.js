68999283848dde83d2f4fde7e6589e855982716a
Lucas Silverio <117397454+lukasilverio94@users.noreply.github.com>
2023-06-23 02:41:25 +0200

    Added some Javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,57 @@
-console.log('teste')
+const data = [
+  {
+    min: 0,
+    max: 18.4,
+    classification: "Menor que 18,5",
+    info: "Magreza",
+    obesity: "0",
+  },
+  {
+    min: 18.5,
+    max: 24.9,
+    classification: "Entre 18,5 e 24,9",
+    info: "Normal",
+    obesity: "0",
+  },
+  {
+    min: 25,
+    max: 29.9,
+    classification: "Entre 25,0 e 29,9",
+    info: "Sobrepeso",
+    obesity: "I",
+  },
+  {
+    min: 30,
+    max: 39.9,
+    classification: "Entre 30,0 e 39,9",
+    info: "Obesidade",
+    obesity: "II",
+  },
+  {
+    min: 40,
+    max: 99,
+    classification: "Maior que 40,0",
+    info: "Obesidade grave",
+    obesity: "III",
+  },
+];
+
+// Selecao de elementos
+const imcTable = document.querySelector("#imc-table");
+
+const heightInput = document.querySelector("#height");
+const weightInput = document.querySelector("#weight");
+const calcBtn = document.querySelector("#calc-btn");
+const clearBtn = document.querySelector("#clear-btn");
+
+// funcoes:
+function createTable(data) {
+  data.forEach((item) => {
+
+    const div = document.createElement("div");
+    div.classList.add("table-data");
+  });
+}
+//inicializacao do meu projeto
+
+//Eventos qdo os botoes comecam funcionar baseado nos inputs do usuario
